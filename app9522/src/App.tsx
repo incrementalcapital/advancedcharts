4d489110983119a361adb4374ec8afee0d062e2a
incremental capital <incrementalcapital@gmail.com>
2024-08-09 18:58:01 +0900

    need a rollback

--- app9522/src/App.tsx
+++ app9522/src/App.tsx
@@ -1,38 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import { Amplify, Auth } from 'aws-amplify';
-import { Authenticator } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
-import awsExports from './aws-exports';
+import React from 'react';
 import TradingViewWidget from './TradingViewWidget';
-
-Amplify.configure(awsExports);
+import './App.css';
 
 function App() {
-  const [user, setUser] = useState<any>(null);
-
-  useEffect(() => {
-    checkUser();
-  }, []);
-
-  async function checkUser() {
-    try {
-      const currentUser = await Auth.currentAuthenticatedUser();
-      setUser(currentUser);
-    } catch (err) {
-      setUser(null);
-    }
-  }
-
   return (
-    <Authenticator>
-      {({ signOut, user }) => (
-        <div className="App" style={{ height: '100vh', width: '100%' }}>
-          <h1>Welcome, {user?.username}!</h1>
-          <button onClick={signOut}>Sign out</button>
-          <TradingViewWidget />
-        </div>
-      )}
-    </Authenticator>
+    <div className="App" style={{ height: '100vh', width: '100%' }}>
+      <TradingViewWidget />
+    </div>
   );
 }
 
